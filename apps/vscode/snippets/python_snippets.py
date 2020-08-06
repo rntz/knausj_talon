2255ac74b5b862a5dada79f7219e810b1743e931
knausj85 <knaus.jeff@gmail.com>
2020-08-05 18:28:04 -0600

    Initial snippet watcher.

--- apps/vscode/snippets/python_snippets.py
+++ apps/vscode/snippets/python_snippets.py
@@ -1,4 +1,6 @@
 from talon import Context, actions, ui, Module, app
+from ....code.snippet_watcher import snippet_watcher
+import os
 
 ctx = Context()
 ctx.matches = r"""
@@ -11,18 +13,20 @@ mode: user.python
 mode: command 
 and code.language: python
 """
-# short name -> ide clip name
-ctx.lists["user.snippets"] = {
-    "class funky": "def(class method)",
-    "class static funky": "def(class static method)",
-    "class": "class",
-    "else if": "elif",
-    "for": "for",
-    "funky": "def",
-    "if else": "if/else",
-    "if": "if",
-    "lambda": "lambda",
-    "try except": "try/except",
-    "while": "while",
-    "with": "with",
-}
+
+
+def update_list(watch_list):
+    ctx.lists["user.snippets"] = watch_list
+
+
+# there's probably a way to do this without
+if app.platform == "windows":
+    watcher = snippet_watcher(
+        {
+            os.path.expandvars(r"%AppData%\Code\User\snippets"): ["python.json"],
+            os.path.expandvars(
+                r"%USERPROFILE%\.vscode\extensions\ms-python.python-2020.7.96456\snippets"
+            ): ["python.json"],
+        },
+        update_list,
+    )
---
+++ code/snippet_watcher.py
@@ -0,0 +1,89 @@
+from talon import app, fs
+import os, json, csv, re
+from os.path import isfile, join
+from itertools import islice
+from pathlib import Path
+
+pattern = re.compile(r"[A-Z][a-z]*|[a-z]+|\d")
+
+# todo: should this be an action that lives elsewhere??
+def create_spoken_form(text, max_len=15):
+    return " ".join(list(islice(pattern.findall(text), max_len)))
+
+
+class snippet_watcher:
+    directories = {}
+    snippet_dictionary = {}
+    callback_function = None
+
+    def __update_all_snippets(self):
+        self.snippet_dictionary = {}
+        for directory, file_list in self.directories.items():
+            if os.path.isdir(directory):
+                for f in file_list:
+                    path = os.path.join(directory, f)
+                    self.__process_file(path)
+
+        # print(str(self.snippet_dictionary))
+
+        if self.callback_function:
+            self.callback_function(self.snippet_dictionary)
+
+    def __process_file(self, name):
+        path_obj = Path(name)
+        directory = os.path.normpath(path_obj.parents[0])
+        file_name = path_obj.name
+        file_type = path_obj.suffix
+
+        # print("{}, {}, {}, {}".format(name, directory, file_name, file_type))
+        if directory in self.directories and file_name in self.directories[directory]:
+            if file_type.lower() == ".json":
+                jsonDict = {}
+
+                if os.path.isfile(name):
+                    with open(name, "r") as f:
+                        jsonDict = json.loads(
+                            "\n".join(
+                                [
+                                    row
+                                    for row in f.readlines()
+                                    if len(row.split("//")) == 1
+                                ]
+                            )
+                        )
+                # else:
+                #     print("snippet_watcher.py: File {}  does not exist".format(directory))
+
+                for key, data in jsonDict.items():
+                    self.snippet_dictionary[create_spoken_form(key)] = data["prefix"]
+
+    def __on_fs_change(self, name, flags):
+        self.__process_file(name)
+
+        print(str(self.snippet_dictionary))
+        if self.callback_function:
+            self.callback_function(self.snippet_dictionary)
+
+    def __init__(self, dirs, callback):
+        self.directories = dirs
+        self.callback_function = callback
+
+        # none = process all directories
+        self.__update_all_snippets()
+
+        for directory in self.directories.keys():
+            if os.path.isdir(directory):
+                fs.watch(directory, self.__on_fs_change)
+            # else:
+            #     print(
+            #         "snippet_watcher.py: directory {} does not exist".format(directory)
+            #     )
+
+
+# Test = snippet_watcher(
+#     {os.path.expandvars(r"%AppData%\Code\User\snippets"): ["python.json"]},
+#     None
+#     # {os.path.expandvars(r"%USERPROFILE%\.vscode\extensions\ms-dotnettools.csharp-1.22.1\snippets": ["csharp.json"]},
+# )
+# print(str(Test.directories))
+
